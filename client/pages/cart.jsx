12901a8885a5b8bdaabb4facf1d8adb75e5cface
neumanf <fabricionewman@protonmail.com>
2021-07-25 13:04:38 +0000

    feat: list products on cart and calculate total

--- client/pages/cart.jsx
+++ client/pages/cart.jsx
@@ -1,50 +1,124 @@
 import { observer } from "mobx-react";
-import {CartStore} from "stores/cartStore"
-
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import cartStore from "stores/cartStore";
 
 function Cart() {
-    return (
-        
+    const [quantities, setQuantities] = useState({});
+    const [total, setTotal] = useState(0);
 
-        <div className="container">
-            <h1>Carrinho</h1>
+    function handleChange(e, price) {
+        setQuantities({
+            ...quantities,
+            [e.target.name]: { price, quantity: e.target.value },
+        });
+    }
+
+    // Inicializa quantities com o conteúdo do carrinho ao carregar a página
+    useEffect(() => {
+        let newQuantities = {};
+
+        cartStore.cart.forEach((product) => {
+            newQuantities = {
+                ...newQuantities,
+                [product._id]: { price: product.price, quantity: "1" },
+            };
+        });
+
+        setQuantities(newQuantities);
+    }, []);
+
+    // Calcula um novo total ao modificar a quantidade de um produto
+    useEffect(() => {
+        let newTotal = 0;
+
+        for (const [id, info] of Object.entries(quantities)) {
+            newTotal += info.price * Number(info.quantity);
+        }
+
+        setTotal(newTotal);
+    }, [quantities]);
+
+    return (
+        <div className="container py-10">
+            <h1 className="text-2xl font-bold">Carrinho</h1>
+
+            <button onClick={() => console.log(quantities)}>quantities</button>
 
             <div className="principal">
                 <div className="lista-produtos">
-                    <div className="produto">
-                        <div className="imagem-produto"><img src="" alt="" srcset="" /></div>
-                        <div className="descricao-produto">
-                            <h4 id="produto-titulo">Titulo</h4>
-                            <p id="produto-preco">R$</p>
-                        </div>
-                        <div className="quantidade">
-                            <div className="quantidade-text">Quantidade</div>
-                            <div className="quantidade-box">
-                                <p>00</p>
+                    {Object.entries(cartStore.cart).length !== 0 ? (
+                        cartStore.cart.map((product) => (
+                            <div className="produto">
+                                <div className="imagem-produto">
+                                    <img
+                                        src={product.image_url}
+                                        alt="produto selecionado"
+                                    />
+                                </div>
+                                <div className="descricao-produto">
+                                    <h4 className="produto-titulo text-purpled">
+                                        {product.title}
+                                    </h4>
+                                    <p className="produto-preco text-purpled">
+                                        <span className="text-gray-400 mr-2 font-medium">
+                                            Preço
+                                        </span>
+                                        R$ {product.price}
+                                    </p>
+                                </div>
+                                <div className="quantidade">
+                                    <div className="quantidade-text">
+                                        Quantidade
+                                    </div>
+                                    <div className="quantidade-box">
+                                        <input
+                                            type="number"
+                                            name={product._id}
+                                            class="w-32 rounded outline-none text-center text-purpled"
+                                            defaultValue="1"
+                                            min="1"
+                                            onChange={(e) =>
+                                                handleChange(e, product.price)
+                                            }
+                                        />
+                                    </div>
+                                </div>
                             </div>
+                        ))
+                    ) : (
+                        <div>
+                            <p>Seu carrinho está vazio.</p>
                         </div>
-                    </div>
+                    )}
                 </div>
+
                 <div className="compras-dados">
                     <div className="cep">
                         <div className="text-cep">CEP</div>
-                        <input className="cep-input" type="text" />
+                        <input
+                            className="cep-input"
+                            placeholder="00000-000"
+                            type="text"
+                        />
                     </div>
                     <div className="total">
                         <div className="text-total">Total</div>
-                        <div className="valor-total">R$ 0.0</div>
+                        <div className="valor-total">R$ {total}</div>
                     </div>
                     <div className="finalizar">
-                        <button type="button" className="button-finalizar"> Finalizar Compras </button>
+                        <button
+                            type="button"
+                            className="button-finalizar"
+                            onClick={() => toast.success("Compra finalizada")}
+                        >
+                            {" "}
+                            Finalizar Compra{" "}
+                        </button>
                     </div>
                 </div>
             </div>
-
-
-
         </div>
-
-        
     );
 }
 
